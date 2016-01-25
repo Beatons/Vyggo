ddab3bb0b55fbf57f45ab5e82574dff0a79d41b5
goatic <goatic@live.dk>
2016-01-25 15:52:18 +0100

    fixed form in modal... for now

--- client/view/components/loginModal.js
+++ client/view/components/loginModal.js
@@ -1,14 +1,17 @@
 Template.loginModal.events({
 	'submit #login-form'(e, t){
 		e.preventDefault()
-
+		
 		let identity = e.target.identity,
 			password = e.target.password
 
-		Meteor.loginWithPassword(identity.value, password.value, function(error){
-			if (error)
-				return
+		Meteor.loginWithPassword(identity.value, password.value, error => {
+			if(error)
+				return console.log(error)
+
 			Meteor.logoutOtherClients()
+
+			$('#login-modal').modal('hide')
 		})
 	}
 })
